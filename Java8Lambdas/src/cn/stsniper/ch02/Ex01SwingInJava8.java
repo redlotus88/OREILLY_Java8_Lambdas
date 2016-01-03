15d2c9c6195e09cab0a8b0c1e0c05e9dc23083c6
RdLts <illidan70@163.com>
2016-01-03 15:46:27 +0800

    Example for chapter 02

---
+++ Java8Lambdas/src/cn/stsniper/ch02/Ex01SwingInJava8.java
@@ -0,0 +1,32 @@
+package cn.stsniper.ch02;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Ex01SwingInJava8 {
+	
+	public static void main(String[] args) {
+		JFrame jFrame = new JFrame("Swing in java 8!");
+		jFrame.setLayout(new FlowLayout());
+		jFrame.setBounds(200, 200, 200, 200);
+	
+		JButton jButton1 = new JButton("Click Me!");
+		
+		//Before java 8, how we add a action listener for button:
+//		jButton1.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				System.out.println(System.out.println("button clicked"));
+//			}
+//		});
+		
+		//After java 8, how we add a action listener for button:
+		jButton1.addActionListener((actionEvent) -> {System.out.println("button clicked");});
+		
+		jFrame.add(jButton1);
+		
+		jFrame.setVisible(true);
+	}
+}
